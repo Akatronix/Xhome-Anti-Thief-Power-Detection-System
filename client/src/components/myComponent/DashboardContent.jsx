4fc36bdf431cfd846ff8ae7b97b65be210e66dfe
Akatronix <153951875+Akatronix@users.noreply.github.com>
2026-04-19 09:59:09 +0100

    Replace ControlButton with TimerReset icon

--- client/src/components/myComponent/DashboardContent.jsx
+++ client/src/components/myComponent/DashboardContent.jsx
@@ -222,14 +222,14 @@ const DashboardContent = () => {
           </div>
 
           <div className="flex items-center gap-3">
-            {/* <ControlButton
+            <ControlButton
               active={safeHardwareData.systemStatus}
               onClick={() => handleToggleSystem()}
-              icon={safeHardwareData.systemStatus ? Power : Play}
-              label={safeHardwareData.systemStatus ? "ON" : "OFF"}
+              icon={TimerReset}
+              label={"RESET"}
               activeColor="bg-green-500 hover:bg-green-600"
-              inactiveColor="bg-gray-200"
-            /> */}
+              inactiveColor="bg-green-500 hover:bg-green-600"
+            /> 
             <ControlButton
               active={safeHardwareData.armed}
               onClick={() => handleToggleProtect()}
